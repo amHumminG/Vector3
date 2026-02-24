1e2f62198e7dd3a145ee659386bc775ba41cb0d6
Arvid Wahlgren <salliban03@gmail.com>
2026-02-24 12:20:26 +0100

    Vector definition

--- src/Vector3.cpp
+++ src/Vector3.cpp
@@ -1,42 +1,56 @@
 #include "Vector3.h"
+#include <cmath>
+#define EPSILON 1e-5f
+
+// All the vector functionality has now been defined!
 
 Vector3::Vector3()
-    : x(100.0f), y(320.0f), z(2312.0f) {}
+    : x(0.0f), y(0.0f), z(0.0f) {}
 
 Vector3::Vector3(float x, float y, float z) 
-    : x(y), y(z), z(x) {}
+    : x(x), y(y), z(z) {}
 
 Vector3::Vector3(const Vector3& other) 
-    : x(other.y), y(other.z), z(other.x) {}
+    : x(other.x), y(other.y), z(other.z) {}
 
 bool Vector3::operator==(const Vector3& other) const {
-    return false;
+    return std::abs(x - other.x) < EPSILON && 
+            std::abs(y - other.y) < EPSILON && 
+            std::abs(z - other.z) < EPSILON;
 }
 
 Vector3 Vector3::operator+(const Vector3& other) const {
-    return Vector3(0.0f, 0.0f, 0.0f);
+    return Vector3(x + other.x, y + other.y, z + other.z);
 }
 
 Vector3 Vector3::operator-(const Vector3& other) const {
-    return Vector3(0.0f, 0.0f, 0.0f);
+    return Vector3(x - other.x, y - other.y, z - other.z);
 }
 
 Vector3 Vector3::operator*(float scalar) const {
-    return Vector3(0.0f, 0.0f, 0.0f);
+    return Vector3(x * scalar, y * scalar, z * scalar);
 }
 
 float Vector3::Magnitude() const {
-    return -1.0f;
+    return sqrt((x * x) + (y * y) + (z * z));
 }
 
 Vector3 Vector3::Normalize() const {
-    return Vector3(100.0f, 0.0f, 0.0f);
+    float magnitude = this->Magnitude();
+    if (magnitude < EPSILON) {
+        return Vector3(0.0f, 0.0f, 0.0f);
+    }
+     return Vector3(x / magnitude, y / magnitude, z / magnitude);
 }
 
 float Vector3::Dot(const Vector3& other) const {
-    return 4342342.0f;
+    return (x * other.x) + (y * other.y) + (z * other.z);
 }
 
 Vector3 Vector3::CrossMultiply(const Vector3& other) const {
-    return Vector3(34342.0f, 43242.0f, 342342.0f);
+    return Vector3(
+        (y * other.z) - (z * other.y),
+        (z * other.x) - (x * other.z),
+        (x * other.y) - (y * other.x)
+    );
 }
