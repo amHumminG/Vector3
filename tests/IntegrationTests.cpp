f809d6e26ba4a1fc1e1fa8feeb919994dd80f40b
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2026-02-24 18:20:19 +0100

    Implemented Integration test

--- tests/IntegrationTests.cpp
+++ tests/IntegrationTests.cpp
@@ -1,2 +1,28 @@
 #include "Vector3.h"
 #include <gtest/gtest.h>
+#include <cmath>
+#define PRECISION 1e-5f
+
+void TestProjRefl() {
+    // Setup
+    Vector3 vecA(-7.0f, 4.0f, 1.0f);
+    Vector3 vecB(3.0f, 2.0f, -1.0f);
+
+    // Exercise
+    float nominator = vecA.Dot(vecB);
+    float denominator = pow(vecA.Magnitude(), 2);
+    float scale = nominator / denominator;
+    
+    Vector3 projection = vecA * scale;
+    Vector3 reflection = (2 * projection) - vecB;
+
+    // Verify projection
+    EXPECT_NEAR(projection.x, 1.48484848f, PRECISION);
+    EXPECT_NEAR(projection.y, -0.84848484f, PRECISION);
+    EXPECT_NEAR(projection.z, -0.21212121f, PRECISION);
+
+    // Verify reflection
+    EXPECT_NEAR(reflection.x, 0.03030303f, PRECISION);
+    EXPECT_NEAR(reflection.y, -3.69696969f, PRECISION);
+    EXPECT_NEAR(reflection.z, -0.57575757f, PRECISION);
+}
